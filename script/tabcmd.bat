6b4ab938b41c98b96f86d7ffbd51f58763f50dae
Dharmendra.Mishra <001.dharmendra@gmail.com>
2018-12-21 09:37:49 +0530

    ndp

---
+++ script/tabcmd.bat
@@ -0,0 +1,5 @@
+cd "C:\tabcmd\Command Line Utility"
+
+tabcmd login -s https://10az.online.tableau.com neoogilvy -u simon.mccarthy@ogilvy.com -p Alr3adyH0me
+
+tabcmd get "/views/SageGlobal2018DataAudit/AccountAdvertisersList.csv" -f "C:\mediaops\mapping\advertiserlist\Account_&_Advertisers_List_data.csv"
