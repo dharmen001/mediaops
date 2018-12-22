63a7b580e479b10230d2770792db7eafa75b3483
Dharmendra.Mishra <001.dharmendra@gmail.com>
2018-12-22 17:46:54 +0530

    ndp

--- htmlfiles/webapp.py
+++ htmlfiles/webapp.py
@@ -1,3 +1,4 @@
+from Classes import OutlookNdpDownload
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -5,6 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route('/Submit')
+def submit():
+    object_outlook = OutlookNdpDownload.Outlook()
+    object_outlook.main()
+    return 'Report Generated'
 
 
 if __name__ == "__main__":
