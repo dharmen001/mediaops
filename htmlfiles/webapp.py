093136461f612871bd207f5f2ac1ebdf00ee033c
Dharmendra.Mishra <001.dharmendra@gmail.com>
2019-03-30 12:54:25 +0530

    Sizmek API Tested

--- htmlfiles/webapp.py
+++ htmlfiles/webapp.py
@@ -1,26 +1,31 @@
 # coding=utf-8
 # !/usr/bin/env python
 
-from Classes.DataReaders import OutlookNdpDownload
-from flask import Flask, render_template
-from Classes.NdpTickets.Advertiser_list import AdvertiserList
-from Classes.NdpTickets.JiraWrapper import Jira
-from Classes.DataReaders.config_ini import Config
+
+from flask import Flask, render_template, request
 import subprocess
+
 app = Flask(__name__)
+
+# @ signifies a decorator - way to wrap a function and modifying its behaviour
 
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/home")
+def home():
+    return render_template('FlaskApp.html')
 
 
 @app.route('/Submit', methods=['POST'])
 def submit():
-    subprocess.call([r'C:\mediaops\script\Ndp.bat'])
-    # object_outlook = OutlookNdpDownload.Outlook()
-    # object_outlook.main()
-    return 'Report Generated'
+    report = request.form['reports']
+
+    if report == 'Generate Jira Tickets':
+        subprocess.call([r'C:\mediaops\script\GenerateJiraTickets.bat'])
+        return 'Generate Jira Tickets submitted'
+    elif report == 'NDP Reports':
+        subprocess.call([r'C:\mediaops\script\Ndp.bat'])
+        return 'NDP Reports Generated'
 
 
 if __name__ == "__main__":
