ba6f932e80300a3b7b14ece5601fe2f344d1d59a
Dharmendra.Mishra <001.dharmendra@gmail.com>
2019-03-31 23:28:41 +0530

    Sizmek API Tested

--- Classes/DataReaders/OutlookEmailSend.py
+++ Classes/DataReaders/OutlookEmailSend.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 # !/usr/bin/env python
 import smtplib
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from Classes.LoggerFile.LogFile import logger
 from Classes.DataReaders.config_ini import Config
-import datetime
+from string import Template
+from email import Encoders
 import pandas as pd
 
 
@@ -13,41 +14,60 @@ class SendEmail(Config):
 
     def __init__(self):
         super(SendEmail, self).__init__()
-        self.outlook_recipient = None
-        self.outlook_subject = None
+        self.username = self.section_value[5]
+        self.password = self.section_value[6]
+        self.file_name = None
 
-    def read_outlook_file(self):
-        outlook_file = pd.read_csv(self.section_value[9] + "outlookReciepientsList.csv")
-        outlook_recipient = outlook_file['Recipient']
-        outlook_subject = outlook_file['Subject']
-        self.outlook_recipient = outlook_recipient
-        self.outlook_subject = outlook_subject
+    def get_contacts(self, filename):
+        """
+            Return two lists names, emails containing names and email addresses
+            read from a file specified by filename.
+        """
 
-    def send_mail(self, recipient, subject, message):
-        logger.info("Start Sending ")
-        username = self.section_value[5]
-        password = self.section_value[6]
-
-        msg = MIMEMultipart()
-        msg['From'] = username
-        msg['To'] = recipient
-        msg['Subject'] = subject
-        msg.attach(MIMEText(message))
-        print('sending mail to ' + recipient + ' on ' + subject)
-        mailServer = smtplib.SMTP('smtp-mail.outlook.com', 587)
-        mailServer.ehlo()
-        mailServer.starttls()
-        mailServer.ehlo()
-        mailServer.login(username, password)
-        mailServer.sendmail(username, recipient, msg.as_string())
-        mailServer.close()
+        file_name = pd.read_csv(filename)
+        body = file_name['Body']
+        names = file_name['Name']
+        emails = file_name['Email']
+        subjects = file_name['Subject']
+        attachments = file_name['FileName']
+        self.file_name = filename
+        return body, names, emails, subjects, attachments
 
     def main(self):
-        self.read_outlook_file()
-        self.send_mail('Adops-India@neomediaworld.com',
-                       "{}".format(), "Start Creating Report for {} ".format() +
-                       str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")) + "." + "\n" + "\n" +
-                       "Thanks\nAdops-Team")
+        body, names, emails, subjects, attachments = self.get_contacts('mycontacts.csv')  # read contacts
+
+        # set up the SMTP server
+        s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(self.username, self.password)
+
+        # For each contact, send the email:
+        for msg_body, name, email, subject, attachment in zip(body, names, emails, subjects, attachments):
+            file_path = 'C:/Users/dharmendra.mishra/OneDrive - insidemedia.net/Reports/Monday/Mazda_Report/'
+            msg = MIMEMultipart()  # create a message
+
+            # setup the parameters of the message
+            msg['From'] = self.username
+            msg['To'] = email
+            msg['Subject'] = subject
+
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(open(file_path + attachment, 'rb').read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment', filename=attachment)
+
+            msg.attach(part)
+
+            # add in the message body
+            msg.attach(MIMEText(msg_body, 'plain'))
+
+            # send the message via the server set up earlier.
+            s.sendmail(msg['From'], msg['To'], msg.as_string())
+
+        # Terminate the SMTP session and close the connection
+        s.close()
 
 
 if __name__ == "__main__":
