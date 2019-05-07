b289d5769e14bf5ca8d055411d7d3a4f72f7b5b3
Dharmendra.Mishra <001.dharmendra@gmail.com>
2019-05-07 18:47:21 +0530

    Project Deployed

--- script/NdpMonthEndProcess.sh
+++ script/NdpMonthEndProcess.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #!/bin/sh
-python /home/groupm/mediaops-project/mediaops/Classes/ProcessEnd/MainNDPMonthENDProcess.py
+python /home/groupm/mediaops-project/mediaops/Classes/ProcessEnd/MainNDPMonthEndProcess.py
