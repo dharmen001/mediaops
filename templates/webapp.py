24fcd9ace7e09c5d789b2d726be7faf7810e1dd2
Dharmendra.Mishra <001.dharmendra@gmail.com>
2019-05-07 17:25:20 +0530

    Project Deployed

--- templates/webapp.py
+++ templates/webapp.py
@@ -32,4 +32,10 @@ def submit():
 
 
 if __name__ == "__main__":
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
     app.run(debug=True, host='0.0.0.0',  port=5000)
+    # http_server = WSGIServer(('0.0.0.0', 5000), app)
+    # http_server.serve_forever()
+
