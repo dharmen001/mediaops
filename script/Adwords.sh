901066f04cd80f6ded11f0def53040aa47c903d0
Dharmendra.Mishra <001.dharmendra@gmail.com>
2019-05-13 18:04:07 +0530

    Project Deployed

--- script/Adwords.sh
+++ script/Adwords.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #!/bin/sh
-nohup python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/GetAdwordsCampaign.py
-nohup python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/AdwordsFileReader.py
-nohup python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/RemoveAdwords.py
+python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/GetAdwordsCampaign.py
+python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/AdwordsFileReader.py
+python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/RemoveAdwords.py
