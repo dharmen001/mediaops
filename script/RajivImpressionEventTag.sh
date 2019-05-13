b40959ba24f1656c060ce417a52cabee4247a9cb
Dharmendra.Mishra <001.dharmendra@gmail.com>
2019-05-13 13:52:20 +0530

    Project Deployed

--- script/RajivImpressionEventTag.sh
+++ script/RajivImpressionEventTag.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #!/bin/sh
-python /home/groupm/mediaops-project/mediaops/Classes/DcmPlatform/RajivImpressionEventTag.py
+nohup python /home/groupm/mediaops-project/mediaops/Classes/DcmPlatform/RajivImpressionEventTag.py
--- script/jira.sh
+++ script/jira.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #!/bin/sh
-python /home/groupm/mediaops-project/mediaops/Classes/NdpTickets/MainNDP.py
+nohup python /home/groupm/mediaops-project/mediaops/Classes/NdpTickets/MainNDP.py
