9b0e0ad0c71cb63f8042f15db055bc211ed86a3d
dharmen001 <001.dharmendra@gmail.com>
2019-05-22 18:44:55 +0530

    jira and logfile updated

--- script/jira.sh
+++ script/jira.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 #!/bin/sh
+repos=("/home/groupm/mediaops-project/mediaops/")
+
+echo ""
+echo "Getting latest for" ${#repos[@]} "repositories using pull --rebase"
+
+for repo in "${repos[@]}"
+do
+  echo ""
+  echo "****** Getting latest for" ${repo} "******"
+  cd "${repo}"
+  git pull --rebase
+  echo "******************************************"
+done
 python /home/groupm/mediaops-project/mediaops/Classes/NdpTickets/MainNDP.py
