0c2ea022075b1149ee55cc1e72bc63a9f8f2a264
dharmen001 <001.dharmendra@gmail.com>
2019-05-27 19:05:14 +0530

    adwords file path updated

--- script/Adwords.sh
+++ script/Adwords.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #!/bin/sh
-python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/GetAdwordsCampaign.py
-python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/AdwordsFileReader.py
-python /home/groupm/mediaops-project/mediaops/Classes/PlatformScript/RemoveAdwords.py
+python /home/groupm/mediaops-project/mediaops/Classes/Adwords/GetAdwordsCampaign.py
+python /home/groupm/mediaops-project/mediaops/Classes/Adwords/AdwordsFileReader.py
+python /home/groupm/mediaops-project/mediaops/Classes/Adwords/RemoveAdwords.py
